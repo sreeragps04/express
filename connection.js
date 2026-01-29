6ea686a0a9cf4df85eda0c3baa79d86b9cf8bfca
sreeragps04 <pssreerag2004@gmail.com>
2026-01-29 12:16:02 +0530

    DB added

--- connection.js
+++ connection.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
  export default async function connection() {
-    const db = await mongoose.connect('mongodb://127.0.0.1:27017/backend')
+    const db = await mongoose.connect(`${process.env.DB_URL}backend`)
     console.log("database connected");
     return db
     
